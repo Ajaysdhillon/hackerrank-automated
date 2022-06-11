f34dfd9c9e24e39c57583c33b5ec3d8d60f81e0d
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-06-11 23:59:02 +0530

    Automating our browser to open hackerrank

--- hk.js
+++ hk.js
@@ -1,12 +1,22 @@
 const puppeteer = require("puppeteer");
 
+const loginLink = "https://www.hackerrank.com/auth/login";
+
 let browserOpen = puppeteer.launch({
   headless: false,
   args: ["--start-maximized"],
   defaultViewport: null,
 });
 
-browserOpen.then(function (browserObj) {
-  let browserOpenPromise = browserObj.newPage();
-  return browserOpenPromise;
-});
+let page;
+
+browserOpen
+  .then(function (browserObj) {
+    let browserOpenPromise = browserObj.newPage();
+    return browserOpenPromise;
+  })
+  .then(function (newTab) {
+    page = newTab;
+    let hackerRankOpenPromise = newTab.goto(loginLink);
+    return hackerRankOpenPromise;
+  });
