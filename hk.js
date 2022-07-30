213818358ecf2b5eb35bb0e7a2df76a0e30f1e78
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-07-30 23:37:19 +0530

    Trying to target the submit button in hackerrank

--- hk.js
+++ hk.js
@@ -151,7 +151,7 @@ function questionSolver(page, question, answer) {
       return ctrlIsUnPressed;
     })
     .then(function () {
-      return page.click(".hr-monaco__run-code", { delay: 50 });
+      return page.click(".hr-monaco-submit.ui-btn-styled", { delay: 50 });
     })
     .then(function () {
       resolve();
