ee4a99c21c42c12d22269dbc5ca5b8c4cc3006a7
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-09-14 23:39:32 +0530

    Targetting the run button

--- hk.js
+++ hk.js
@@ -150,7 +150,7 @@ function questionSolver(page, question, answer) {
       return ctrlIsUnPressed;
     })
     .then(function () {
-      return page.click(".ui-btn ui-btn-normal", { delay: 50 });
+      return page.click(".ui-btn-normal", { delay: 50 });
     })
     .then(function () {
       resolve();
