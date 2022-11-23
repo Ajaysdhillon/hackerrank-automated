682a817edf59adace6647fefc9549b81cc3eb051
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-11-23 23:49:17 +0530

    Trying to target run button

--- hk.js
+++ hk.js
@@ -150,7 +150,7 @@ function questionSolver(page, question, answer) {
       return ctrlIsUnPressed;
     })
     .then(function () {
-      return page.click(".monaco-editor", {
+      return page.click(".monaco-editor-bkas", {
         delay: 50,
       });
     })
