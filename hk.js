1107ada38bf955ef2ebd51cf21eeac39c49b0f45
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-11-27 22:35:30 +0530

    Trying to target run button

--- hk.js
+++ hk.js
@@ -151,7 +151,7 @@ function questionSolver(page, question, answer) {
     })
     .then(function () {
       return page.click(
-        ".ui-btn ui-btn-normal ui-btn-secondary pull-right msR hr-monaco-compile hr-monaco__run-code ui-btn-styled",
+        ".ui-btn ui-btn-normal ui-btn-secondary pull-right msR hr-monaco-compile hr-monaco_",
         {
           delay: 50,
         }
