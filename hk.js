3c789e1c91c7a10c0a5ecde112fc81866d511821
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-12-01 23:38:00 +0530

    Trying to target run button

--- hk.js
+++ hk.js
@@ -151,7 +151,7 @@ function questionSolver(page, question, answer) {
     })
     .then(function () {
       return page.click(
-        ".ui-btn ui-btn-normal ui-btn-secondary pull-right",
+        ".ui-btn ui-btn-normal ui-btn-seconda",
         {
           delay: 50,
         }
