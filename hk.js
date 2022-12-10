29e78d9d67975ac9b1ad967f34f58aacc25e8b50
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-12-10 23:45:08 +0530

    Trying to target run button

--- hk.js
+++ hk.js
@@ -150,7 +150,7 @@ function questionSolver(page, question, answer) {
       return ctrlIsUnPressed;
     })
     .then(function () {
-      return page.click(".u ", {
+      return page.click(". ", {
         delay: 50,
       });
     })
