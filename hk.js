361775c98f1ec4681bc14be1e710db871ee9a2bb
Ajaysdhillon <ajaysdhillon@gmail.com>
2023-01-21 23:41:25 +0530

    Targetting run button

--- hk.js
+++ hk.js
@@ -151,7 +151,7 @@ function questionSolver(page, question, answer) {
     })
     .then(function () {
       return page.click(
-        ".butoncustom_buton sekcsjcnspja adcadca tfukhkjhkfg adkjsldacjljdlak shfgfj adkjk;ajkaj  sidjcsihd jandjan sdk;sdj;sk ",
+        ".butoncustom_buton gyg sekcsjcnspja adcadca tfukhkjhkfg adkjsldacjljdlak shfgfj adkjk;ajkaj  sidjcsihd jandjan sdk;sdj;sk ",
         {
           delay: 50,
         }
